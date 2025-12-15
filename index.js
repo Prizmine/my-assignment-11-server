20d0405715e999dd45dd5285d612142aae5e509b
Prizmine <prizmine123@gmail.com>
2025-12-15 10:03:51 +0600

    added roles related apis

--- index.js
+++ index.js
@@ -16,30 +16,23 @@ admin.initializeApp({
 app.use(express.json());
 app.use(cors());
 
-const verifyAdmin = async (req, res, next) => {
-  const user = await usersCollection.findOne({ email: req.user.email });
-  if (user?.role !== "admin") {
-    return res.status(403).send({ message: "Admin only" });
-  }
-  next();
-};
-
 const verifyToken = async (req, res, next) => {
   const authorization = req.headers.authorization;
-  if (!authorization) return res.status(401).send({ message: "unauthorized access" });
 
-  const token = authorization.split(" ")[1];
-  if (!token) return res.status(401).send({ message: "Token missing" });
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
 
   try {
+    const token = authorization.split(" ")[1];
     const decoded = await admin.auth().verifyIdToken(token);
-    if (!decoded.email) throw new Error("Invalid token");
+    console.log(decoded);
     req.decoded_email = decoded.email;
     next();
   } catch (err) {
-    console.log("Token verification failed:", err.message);
-    return res.status(401).send({ message: "Invalid token", error: err.message });
+    return res.status(401).send(err);
   }
+  // console.log(token);
 };
 
 const uri = `mongodb+srv://contest-hub:${process.env.DB_PASSWORD}@algo-nova.ntsxvj0.mongodb.net/?appName=Algo-Nova`;
@@ -62,7 +55,16 @@ async function run() {
     const contestCollection = db.collection("contests");
     const allRolesCollection = db.collection("roles");
 
-    // contest related apis
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded_email;
+      const user = await allRolesCollection.findOne({ email });
+
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "Admin only" });
+      }
+      next();
+    };
+
 
     app.post("/contests", async (req, res) => {
       const contest = req.body;
@@ -83,7 +85,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/contests", async (req, res) => {
+    app.get("/approved-contests", async (req, res) => {
       const query = {};
       if (req.query.status) {
         query.status = req.query.status;
@@ -92,10 +94,107 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/contests", async (req, res) => {
+      const page = parseInt(req.query.page) || 1;
+      const size = parseInt(req.query.size) || 10;
+      const query = {};
+
+      if (req.query.status) {
+        query.status = req.query.status;
+      }
+
+      if (req.query.email) {
+        query.creatorEmail = req.query.email;
+      }
+      const skip = (page - 1) * size;
+
+      try {
+        const contests = await contestCollection
+          .find(query)
+          .skip(skip)
+          .limit(size)
+          .toArray();
+        const totalContestsCount = await contestCollection.countDocuments(
+          query
+        );
+
+        res.send({
+          contests: contests,
+          count: totalContestsCount,
+          itemsPerPage: size,
+          currentPage: page,
+        });
+      } catch (error) {
+        console.error("Error fetching contests with pagination:", error);
+        res
+          .status(500)
+          .send({ message: "Server error, failed to fetch data." });
+      }
+    });
+
+    app.delete("/contests/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contestCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/edit-contests/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const filter = {
+        _id: new ObjectId(id),
+        creatorEmail: req.body.creatorEmail,
+      };
+
+      const updatedData = {
+        $set: {
+          name: req.body.name,
+          image: req.body.image,
+          description: req.body.description,
+          price: req.body.price,
+          prize: req.body.prize,
+          taskInstruction: req.body.taskInstruction,
+          type: req.body.type,
+          deadline: req.body.deadline,
+        },
+      };
+
+      try {
+        const result = await contestCollection.updateOne(filter, updatedData);
+
+        if (result.matchedCount === 0) {
+          return res.status(404).send({
+            message: "Contest not found or you are not authorized to edit.",
+          });
+        }
+
+        res.send({ modifiedCount: result.modifiedCount });
+      } catch (error) {
+        console.error("Error updating contest:", error);
+        res
+          .status(500)
+          .send({ message: "Internal server error during update." });
+      }
+    });
+
     app.get("/contests/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await contestCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/contests/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status,
+        },
+      };
+      const result = await contestCollection.updateOne(query, updatedDoc);
       res.send(result);
     });
 
@@ -117,8 +216,27 @@ async function run() {
       res.send(result);
     });
 
+
     app.get("/roles", async (req, res) => {
-      const result = await allRolesCollection.find().toArray();
+      const query = {};
+      if (req.query.email) {
+        query.email = req.query.email;
+      }
+
+      const result = await allRolesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.patch("/roles/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const role = req.body.role;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: role,
+        },
+      };
+      const result = await allRolesCollection.updateOne(query, updatedDoc);
       res.send(result);
     });
 
